6b22f6a491cddd88e87dddf74ca74402236fbc2a
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-31 13:52:00 -0700

    add different fortunes and commenting out in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
   $("#survey").submit(function(event){
     event.preventDefault();
+    var luckyArray = [];
     $("input:checkbox[name=lucky-event]:checked").each(function(){
       var lucky = $(this).val();
-      console.log(lucky);
+      luckyArray.push(lucky);
     })
+    // console.log(luckyArray);
+    // console.log(luckyArray.length)
+    var unluckyArray = [];
+    $("input:checkbox[name=unlucky-event]:checked").each(function(){
+      var unlucky = $(this).val();
+      unluckyArray.push(unlucky);
+    })
+    // console.log(unluckyArray);
+    // console.log(unluckyArray.length)
   })
+  
 });
