bb8ab4c14f3b40fa858539f86d8390e1444d27ad
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-31 14:24:30 -0700

    add rest of else if statements code is working

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
   $("#survey").submit(function(event){
     event.preventDefault();
-    var luckyArray = [];
-    $("input:checkbox[name=lucky-event]:checked").each(function(){
-      var lucky = $(this).val();
-      luckyArray.push(lucky);
-    })
-    // console.log(luckyArray);
-    // console.log(luckyArray.length)
-    var unluckyArray = [];
-    $("input:checkbox[name=unlucky-event]:checked").each(function(){
-      var unlucky = $(this).val();
-      unluckyArray.push(unlucky);
-    })
-    // console.log(unluckyArray);
-    // console.log(unluckyArray.length)
-  })
-  
+      var luckyArray = [];
+      $("input:checkbox[name=lucky-event]:checked").each(function(){
+        var lucky = $(this).val();
+        luckyArray.push(lucky);
+      })
+      // console.log(luckyArray);
+      // console.log(luckyArray.length)
+      var unluckyArray = [];
+      $("input:checkbox[name=unlucky-event]:checked").each(function(){
+        var unlucky = $(this).val();
+        unluckyArray.push(unlucky);
+      })
+      // console.log(unluckyArray);
+      // console.log(unluckyArray.length)
+    if (luckyArray.length > unluckyArray.length) {
+      $("#fortune1").show();
+    } else if (luckyArray.length === unluckyArray.length) {
+      $("#fortune2").show();
+    } else {
+      $("#fortune3").show();
+    }
+  });
+    // console.log()
 });
